a145537ddab04b2213351a2cf817bac924464a95
Das953 <das953@gmail.com>
2017-07-06 16:18:30 +0300

    some div swap

--- leftPanel/aboutUs.php
+++ leftPanel/aboutUs.php
@@ -7,6 +7,16 @@
 </head>
 <body>
 <div style="display: inline-flex; flex-direction: column; min-width: 80%;max-width: 80%; margin: 0;">
+    <div>
+        <img src="" class="PhotoComand">
+        <pre style="font-size: 15px; white-space: pre-wrap; text-align: center;">
+                <span style="font-size: 20px; font-weight: 600;">Кузьмин Олег</span>
+                <span>Project manager</span>
+                <span>Тел. +38 (098)774-31-26</span>
+                <span>(Україна,Київ)</span>
+            </pre>
+
+    </div>
     <div class="flex-content">
         <div>
             <img src="" class="PhotoComand">
@@ -16,16 +26,6 @@
                 <span>Software architect</span>
                 <span>Тел. +38 (096)331-27-93</span>
                 <span>(Україна,Вінниця)</span>
-            </pre>
-
-        </div>
-        <div>
-            <img src="" class="PhotoComand">
-            <pre style="font-size: 15px; white-space: pre-wrap; text-align: center;">
-                <span style="font-size: 20px; font-weight: 600;">Кузьмин Олег</span>
-                <span>Project manager</span>
-                <span>Тел. +38 (098)774-31-26</span>
-                <span>(Україна,Київ)</span>
             </pre>
 
         </div>
