f468a5727a799cd691a438550f449c65a306edae
NewBalanse <coocies12@gmail.com>
2017-07-10 16:09:09 +0300

    New Admin Style

--- scripts/AdminLogic.js
+++ scripts/AdminLogic.js
@@ -7,46 +7,31 @@ document.write(
      "<input type='button' value='U' onclick='setUnderline()'/>" +
      "<input type='button' value='Size' onclick='setSize()'/>" +
      "<input type='button' value='http://' onclick='setLink()'>" +
-     */"<input type='button' value='IMG' onclick='setImage()'>" +
-    "<br/>" +"<label class='AdminText' for='frameId'> Заглавие </label>"+ "<br>" +
-    "<input type='textarea' scrolling='no' src='#' id='frameId' name='frameId' placeholder='В этом поле необходимо ввести заголовок вашей статьи.'>" +
-    "<br>" + "<label class='AdminText' for='frameContent'>Полный текст</label>" + "<br>" +
-    "<input type='textarea' scrolling='no' src='#' id='frameContent' name='frameContent' placeholder='В этом поле необходимо ввести основной контент вашей статьи'>" + "<br/>" +
-    "<label class='AdminText' for='framePreview'>Краткое описание</label>" + "<br>" +
-    "<input type='textarea' scrolling='no' src='#' id='framePreview' name='framePreview' placeholder='В этом поле необходимо ввести краткое описание вашей статьи'>" + "<br/>" + "<br/>"
+     */
+    "<div class='flexInput'>" +
+    "<textarea id='frameId' name='frameId' placeholder='В этом поле необходимо ввести заголовок вашей статьи' wrap='soft | hard'></textarea>" +
+    "<label class='AdminText' for='frameId'> Заглавие </label>" +
+    "</div>" +
+    "<div class='flexInput'>" +
+    "<textarea id='frameContent' name='frameContent' placeholder='В этом поле необходимо ввести основной контент вашей статьи' wrap='soft | hard'></textarea>" +
+    "<label class='AdminText' for='frameContent'>Полный текст</label>" +
+    "</div>" +
+    "<div class='flexInput'>" +
+    "<textarea id='framePreview' name='framePreview' placeholder='В этом поле необходимо ввести краткое описание вашей статьи' wrap='soft | hard'></textarea>" +
+    "<label class='AdminText' for='framePreview'>Краткое описание</label>" +
+    "</div>"
 );
 
 var isGecko = navigator.userAgent.toLocaleLowerCase().indexOf("gecko") != -1;
+
 var iframe = (isGecko) ? document.getElementById("frameId") : frames["frameId"];
-var iWin = (isGecko) ? iframe.contentWindow : iframe.window;
-var iDoc = (isGecko) ? iframe.contentDocument : iframe.document;
 
 var frameContent = (isGecko) ? document.getElementById("frameContent") : frames["frameContent"];
-var iWinContent = (isGecko) ? frameContent.contentWindow : frameContent.window;
-var iDocContent = (isGecko) ? frameContent.contentDocument : frameContent.document;
 
 var framePreview = (isGecko) ? document.getElementById("framePreview") : frames["framePreview"];
-var iWinPreview = (isGecko) ? framePreview.contentWindow : framePreview.window;
-var iDocPreview = (isGecko) ? framePreview.contentDocument : framePreview.document;
 
-
-var iHTML = "<html><head></head><body style='border: 0px;'></body> </html>";
-iDoc.open();
-iDoc.write(iHTML);
-iDoc.close();
-
-iDocContent.open();
-iDocContent.write(iHTML);
-iDocContent.close();
-
-iDocPreview.open();
-iDocPreview.write(iHTML);
-iDocPreview.close();
-
-
-iDoc.designMode = 'on';
-iDocContent.designMode = 'on';
-iDocPreview.designMode = 'on';
+var CodeHTML = '';//здесь полное описание плюс заглавие
+var CodePreview = '';//Здесь краткое описание плюс заглавие чтобы смотрелось на траничке красиво
 
 function setImage() {
     iWinContent.focus();
@@ -79,14 +64,15 @@ function setImage() {
 function save() {
     /*var HtmlContent = "<div style='background: green;' class='ThisIsContent'></div>";
      document.getElementById("content").value = iDoc.body.innerHTML;*/
-    ReturnHTMLCode(iDoc.body.innerHTML, 0);
-    ReturnHTMLCode(iDocPreview.body.innerHTML, 2);
-    ReturnHTMLCode(iDocContent.body.innerHTML, 1);
+
+    ReturnHTMLCode(iframe.value, 0);
+    ReturnHTMLCode(frameContent.value, 2);
+    ReturnHTMLCode(framePreview.value, 1);
+
+    alert(CodeHTML);
+    document.body.innerHTML = CodeHTML;
 
 }
-var CodeHTML = '';//здесь полное описание плюс заглавие
-var CodePreview = '';//Здесь краткое описание плюс заглавие чтобы смотрелось на траничке красиво
-
 function ReturnHTMLCode(code, ContentOrNO) {
     var htmlcode = code;
     if (htmlcode == " ")
@@ -96,12 +82,9 @@ function ReturnHTMLCode(code, ContentOrNO) {
         CodeHTML = CreateZ(htmlcode, true);
     else if (ContentOrNO == 1)
         CodeHTML += CreateContent(htmlcode, true);
-    else{
-        CodePreview = CodeHTML;
+    else {
         CodePreview += CreateContent(htmlcode, false);
     }
-    alert(CodeHTML);
-    alert(CodePreview);
 
     /*Здесь возврашаеться по идеи полность стилизированый код как в оборудовании*/
 }
@@ -114,35 +97,10 @@ function CreateZ(htmlcode) {
 
     return BigText;
 }
-function CreateContent(htmlcode, ComeON) {
+function CreateContent(htmlcode) {
     var CodeContent = "<div>" +
-        "<pre style='font-size: 20px; font-weight: 400; white-space: pre-line; text-align: left;'>";
+        "<pre style='font-size: 20px; font-weight: 400; white-space: pre-line; text-align: left;'><span>";
+    CodeContent += htmlcode + "</span>" + "</pre></div>";
 
-    var temp = "<span>";
-
-    var count = 0;
-    for (var i = 0; i < htmlcode.length; i++) {
-        if (htmlcode.substring(i, i + 2) == "</") {
-            i += 5;
-            temp += "</span>";
-        }
-        else if (htmlcode.substring(i, i + 1) == '<') {
-            i += 4;
-            temp += "</span><span>";
-        }
-        else {
-            count++;
-            temp += htmlcode.substring(i, i + 1);
-        }
-    }
-    CodeContent += temp + "</pre></div>";
-
-    if (ComeON == true)
-        return CodeContent;
-    else{
-        if(count > 300)
-            alert("Нужно меньше чем 300 символов для краткого описания");
-        else
-            return CodeContent;
-    }
+    return CodeContent;
 }
